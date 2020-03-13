c82c9cf1da70fcd7b5c60c393e7dc1594764d4b7
itsksaurabh <itsksaurabh@gmail.com>
2020-03-13 18:12:19 +0530

    update comments

--- marketTicker.go
+++ marketTicker.go
@@ -2,16 +2,27 @@ package wazirx
 
 // TickerData holds active market data with all ticker related values
 type TickerData struct {
-	BaseUnit  string  `json:"base_unit"`
-	QuoteUnit string  `json:"quote_unit"`
-	Low       string  `json:"low"`
-	High      string  `json:"high"`
-	Last      string  `json:"last"`
-	Type      string  `json:"type"`
-	Open      float64 `json:"open"`
-	Volume    string  `json:"volume"`
-	Sell      string  `json:"sell"`
-	Buy       string  `json:"buy"`
-	At        int     `json:"at"`
-	Name      string  `json:"name"`
+	// ticker code of base market
+	BaseUnit string `json:"base_unit"`
+	// ticker code of quote asset
+	QuoteUnit string `json:"quote_unit"`
+	// 24 hrs lowest price of base asset
+	Low string `json:"low"`
+	// 24 hrs highest price of base asset
+	High string `json:"high"`
+	// Last traded price in current market
+	Last string `json:"last"`
+	Type string `json:"type"`
+	// Market Open price 24hrs ago
+	Open float64 `json:"open"`
+	// Last 24hrs traded volume
+	Volume string `json:"volume"`
+	// Top ask order price
+	Sell string `json:"sell"`
+	// Top bid order price
+	Buy string `json:"buy"`
+	// Timestamp when ticker information is fetched
+	At int `json:"at"`
+	// Display text of market
+	Name string `json:"name"`
 }
