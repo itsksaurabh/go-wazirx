a80e3305a7c9a323bdca61e537cb0051c8c2a59a
itsksaurabh <itsksaurabh@gmail.com>
2020-03-14 16:28:43 +0530

    - use test client in test files

--- marketTrade_test.go
+++ marketTrade_test.go
@@ -3,14 +3,11 @@ package wazirx_test
 import (
 	"context"
 	"testing"
-
-	wazirx "github.com/itsksaurabh/go-wazirx"
 )
 
 func TestMarketTrade(t *testing.T) {
-	c := wazirx.Client{}
 	market := "btcusdt"
-	_, err := c.MarketTrade(context.Background(), market)
+	_, err := client(t).MarketTrade(context.Background(), market)
 	if err != nil {
 		if err != nil {
 			t.Fatal(err)
