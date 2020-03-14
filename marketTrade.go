7aca118e0ebce585a781fdc03ad039f97cfa877a
itsksaurabh <itsksaurabh@gmail.com>
2020-03-14 13:15:38 +0530

    ./marketTrade.go: - add MarketTrade struct to hold trade history of a perticular market

--- marketTrade.go
+++ marketTrade.go
@@ -1,1 +1,16 @@
 package wazirx
+
+import (
+	"time"
+)
+
+// MarketTrade holds trade history of a perticular market
+type MarketTrade struct {
+	ID        int64       `json:"id"`
+	Price     string      `json:"price"`
+	Volume    string      `json:"volume"`
+	Funds     string      `json:"funds"`
+	Market    string      `json:"market"`
+	CreatedAt time.Time   `json:"created_at"`
+	Side      interface{} `json:"side"`
+}
