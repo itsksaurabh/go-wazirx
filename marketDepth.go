a3f21ee6c2da53875fc6b4d072b44efdcffe3799
itsksaurabh <itsksaurabh@gmail.com>
2020-03-14 12:46:04 +0530

    ./marketDepth.go: - change TimeStamp type from int to time.Time - add custom UnmarshalJson to convert slices into struct and covert Timestamp from `int` to unix timestamp

--- marketDepth.go
+++ marketDepth.go
@@ -2,18 +2,57 @@ package wazirx
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/pkg/errors"
 )
 
+// PriceVolume holds price and volume
+type PriceVolume struct {
+	PRICE  string
+	VOLUME string
+}
+
 // MarketDepth holds orderbook data of a perticular market
 type MarketDepth struct {
-	Timestamp int `json:"timestamp"`
+	Timestamp time.Time `json:"timestamp"`
 	// list order's asks
-	Asks [][]string `json:"asks"`
+	Asks []PriceVolume `json:"asks"`
 	// list order's bids
-	Bids [][]string `json:"bids"`
+	Bids []PriceVolume `json:"bids"`
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// converts slices into struct for better access and
+// coverts Timestamp from `int` to unix timestamp
+func (d *MarketDepth) UnmarshalJSON(data []byte) error {
+	aux := struct {
+		Timestamp int64      `json:"timestamp"`
+		Asks      [][]string `json:"asks"`
+		Bids      [][]string `json:"bids"`
+	}{}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	var tmp PriceVolume
+	for _, ask := range aux.Asks {
+		tmp.PRICE = ask[0]
+		tmp.VOLUME = ask[1]
+		d.Asks = append(d.Asks, tmp)
+	}
+
+	for _, bid := range aux.Bids {
+		tmp.PRICE = bid[0]
+		tmp.VOLUME = bid[1]
+		d.Bids = append(d.Bids, tmp)
+	}
+
+	d.Timestamp = time.Unix(aux.Timestamp, 0)
+	return nil
 }
 
 // MarketDepth returs orderbook of any market.
