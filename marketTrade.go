28dc8e2218e1f435ceff3c9473beef2ff2b8c8fb
itsksaurabh <itsksaurabh@gmail.com>
2020-03-14 22:50:54 +0530

    ./marketTrade.go: - change field type from string to float64 - convert numeric string fields to numeric type while unmarshalling

--- marketTrade.go
+++ marketTrade.go
@@ -2,7 +2,9 @@ package wazirx
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/pkg/errors"
@@ -11,12 +13,35 @@ import (
 // MarketTrade holds trade history of a perticular market
 type MarketTrade struct {
 	ID        int64       `json:"id"`
-	Price     string      `json:"price"`
-	Volume    string      `json:"volume"`
-	Funds     string      `json:"funds"`
+	Price     float64     `json:"price"`
+	Volume    float64     `json:"volume"`
+	Funds     float64     `json:"funds"`
 	Market    string      `json:"market"`
 	CreatedAt time.Time   `json:"created_at"`
 	Side      interface{} `json:"side"`
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// It coverts some string type numeric to numeric types.
+func (t *MarketTrade) UnmarshalJSON(data []byte) error {
+	type Alias MarketTrade
+	aux := &struct {
+		Price  string `json:"price"`
+		Volume string `json:"volume"`
+		Funds  string `json:"funds"`
+		*Alias
+	}{
+		Alias: (*Alias)(t),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	t.Price, _ = strconv.ParseFloat(aux.Price, 64)
+	t.Volume, _ = strconv.ParseFloat(aux.Volume, 64)
+	t.Funds, _ = strconv.ParseFloat(aux.Funds, 64)
+	return nil
 }
 
 // MarketTrade returs trade history of a market.
